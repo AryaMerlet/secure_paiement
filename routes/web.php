5383e536ff4c653d99221694eccf237ee9adcabf
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2024-12-06 12:21:22 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/createusers', [UserController::class, 'create']);
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
